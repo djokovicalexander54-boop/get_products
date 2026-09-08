7e6f9fecf5451cc7be18465e1cab070e99a35988
alexander <djokovicalexander54@gmail.com>
2026-09-08 11:49:20 +0330

    Update site_p.py

--- site_p.py
+++ site_p.py
@@ -70,15 +70,14 @@ def home():
         image = image_list
     )
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=7737231906 , text="سلام. در این ربات میتوانید محصولات موبایل سایت دیجی کالا را مشاهده کنید")
-    reply = InlineKeyboardMarkup([[InlineKeyboardButton(text="مشاهده محصولات", url="https://telegold.ir")]])
-    await context.bot.send_message(chat_id=7737231906 , text="برای مشاهده محصولات، کلیک کنید", reply_markup=reply)
+    reply = InlineKeyboardMarkup([[InlineKeyboardButton(text="برای مشاهده محصولات، کلیک کنید", url="https://telegold.ir")]])
+    await context.bot.send_message(chat_id=update.effective_chat.id , text="سلام. در این ربات میتوانید محصولات موبایل سایت دیجی کالا را مشاهده کنید", reply_markup=reply)
 async def start1(update: Update ,context:ContextTypes.DEFAULT_TYPE):
     if update.message and 'send_text' in update.message.text:
         id_name = update.effective_user.first_name
-        await context.bot.send_message(chat_id=7737231906 , text=f"کاربر {id_name}، شما از سایت محصولات موبایل به اینجا هدایت شدید")
+        await context.bot.send_message(chat_id=update.effective_chat.id , text=f"کاربر {id_name}، شما از سایت محصولات موبایل به اینجا هدایت شدید")
     else:
-        await context.bot.send_message(chat_id=7737231906 , text="این ربات صرفا برای تست است")
+        await context.bot.send_message(chat_id=update.effective_chat.id , text="این ربات صرفا برای تست است")
 def run_bot():
     port = int(os.environ.get("PORT",80))
     app.run(host="0.0.0.0", port=port, debug=False)
