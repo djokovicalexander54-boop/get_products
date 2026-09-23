864bbc31d4cd044af7d3745e2853a0673603ef24
alexander <djokovicalexander54@gmail.com>
2026-09-23 17:42:05 +0330

    Update site_p.py

--- site_p.py
+++ site_p.py
@@ -1,9 +1,17 @@
 from google import genai
+import time
+m=1
 key_gemini = "AQ.Ab8RN6LE1xYxM_ocOW91pnCs-7ZTrYwsQw1cK2WVQKn3SpsXdA"
 gemini_ai = genai.Client(api_key=key_gemini)
 #pdf_file_A = gemini_ai.files.upload(file=MM)
-responce = gemini_ai.models.generate_content( 
-    model = f"gemini-3.5-flash", 
-    contents=["HELLO"]
-)
-print(responce.text, flush=True)
+while True:
+    try:
+        responce = gemini_ai.models.generate_content( 
+            model = f"gemini-3.5-flash", 
+            contents=["HELLO"]
+        )
+        print(responce.text, flush=True)
+    except:
+        time.sleep(2)
+        print(f"NO {m}")
+        m+=1
