420a073183cd6a750b0874e203dd1eaa3efab559
alexander <djokovicalexander54@gmail.com>
2026-09-23 17:52:43 +0330

    Update site_p.py

--- site_p.py
+++ site_p.py
@@ -2,9 +2,12 @@ from google import genai
 import time
 from flask import Flask
 import os
+import threading
 app = Flask(__name__)
 @app.route("/")
 def home():
+    return "run"
+def bb():
     m=1
     key_gemini = "AQ.Ab8RN6LE1xYxM_ocOW91pnCs-7ZTrYwsQw1cK2WVQKn3SpsXdA"
     gemini_ai = genai.Client(api_key=key_gemini)
@@ -22,5 +25,6 @@ def home():
             print(f"NO {m}")
             m+=1
 if __name__ == '__main__':
+    threading.Thread(target=bb, daemon=True).start()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
