c5a5228d31f43afd0b06cf860e8788481beb53af
alexander <djokovicalexander54@gmail.com>
2026-09-23 17:47:05 +0330

    Update site_p.py

--- site_p.py
+++ site_p.py
@@ -1,17 +1,26 @@
 from google import genai
 import time
-m=1
-key_gemini = "AQ.Ab8RN6LE1xYxM_ocOW91pnCs-7ZTrYwsQw1cK2WVQKn3SpsXdA"
-gemini_ai = genai.Client(api_key=key_gemini)
-#pdf_file_A = gemini_ai.files.upload(file=MM)
-while True:
-    try:
-        responce = gemini_ai.models.generate_content( 
-            model = f"gemini-3.5-flash", 
-            contents=["HELLO"]
-        )
-        print(responce.text, flush=True)
-    except:
-        time.sleep(2)
-        print(f"NO {m}")
-        m+=1
+from flask import Flask
+import os
+app = Flask(__name__)
+@app.route("/")
+def home():
+    m=1
+    key_gemini = "AQ.Ab8RN6LE1xYxM_ocOW91pnCs-7ZTrYwsQw1cK2WVQKn3SpsXdA"
+    gemini_ai = genai.Client(api_key=key_gemini)
+    #pdf_file_A = gemini_ai.files.upload(file=MM)
+    while True:
+        try:
+            responce = gemini_ai.models.generate_content( 
+                model = f"gemini-3.5-flash", 
+                contents=["HELLO"]
+            )
+            print(responce.text, flush=True)
+            break
+        except:
+            time.sleep(2)
+            print(f"NO {m}")
+            m+=1
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
