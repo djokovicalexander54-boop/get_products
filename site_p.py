22c058b3601ebcfedf53d27ecf2564b7615ac2aa
alexander <djokovicalexander54@gmail.com>
2026-09-24 09:01:18 +0330

    Update site_p.py

--- site_p.py
+++ site_p.py
@@ -125,6 +125,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                         title = str(RRR["widget_list"][0]["data"]["title"]) 
                         number = str(RRR["widget_list"][0]["data"]["value"])
                         phone = f"{title} : {number}"
+                        print(phone)
                     except:
                         try:
                             prox = proxy_list[random.randint(0,15)]
@@ -133,6 +134,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                             title = str(RRR["widget_list"][0]["data"]["title"]) 
                             number = str(RRR["widget_list"][0]["data"]["value"])
                             phone = f"{title} : {number}"
+                            print(phone)
                             await asyncio.sleep(random.randint(1,5))
                         except:
                             phone = "اطلاعات تماس یافت نشد"
